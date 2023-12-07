e25992f04a895d6c65e4cfb392ee95d24ebc44d4
Surya Kusjanto <skusjanto@outlook.com>
2023-12-07 12:16:12 -0700

    Added character movement, collisions, text effects, cutscenes, and screen transitions. Moved status to bottom

--- Overworld.js
+++ Overworld.js
@@ -3,53 +3,84 @@ class Overworld {
    this.element = config.element;
    this.canvas = this.element.querySelector(".game-canvas");
    this.ctx = this.canvas.getContext("2d");
+   this.map = null;
+ }
+
+  startGameLoop() {
+    const step = () => {
+      //Clear off the canvas
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      //Establish the camera person
+      const cameraPerson = this.map.gameObjects.hero;
+
+      //Update all objects
+      Object.values(this.map.gameObjects).forEach(object => {
+        object.update({
+          arrow: this.directionInput.direction,
+          map: this.map,
+        })
+      })
+
+      //Draw Lower layer
+      this.map.drawLowerImage(this.ctx, cameraPerson);
+
+      //Draw Game Objects
+      Object.values(this.map.gameObjects).sort((a,b) => {
+        return a.y - b.y;
+      }).forEach(object => {
+        object.sprite.draw(this.ctx, cameraPerson);
+      })
+
+      //Draw Upper layer
+      this.map.drawUpperImage(this.ctx, cameraPerson);
+      
+      requestAnimationFrame(() => {
+        step();   
+      })
+    }
+    step();
+ }
+
+ bindActionInput() {
+   new KeyPressListener("Enter", () => {
+     //Is there a person here to talk to?
+     this.map.checkForActionCutscene()
+   })
+ }
+
+ bindHeroPositionCheck() {
+   document.addEventListener("PersonWalkingComplete", e => {
+     if (e.detail.whoId === "hero") {
+       //Hero's position has changed
+       this.map.checkForFootstepCutscene()
+     }
+   })
+ }
+
+ startMap(mapConfig) {
+  this.map = new OverworldMap(mapConfig);
+  this.map.overworld = this;
+  this.map.mountObjects();
  }
 
  init() {
-   const image = new Image();
-   image.onload = () => {
-     this.ctx.drawImage(image,0,0)
-   };
-   image.src = "/images/maps/DemoLower.png";
+  this.startMap(window.OverworldMaps.DemoRoom);
 
 
-   const x = 5;
-   const y = 6;
+  this.bindActionInput();
+  this.bindHeroPositionCheck();
 
-   const shadow = new Image();
-   shadow.onload = () => {
-    this.ctx.drawImage(
-      shadow, 
-      0, //left cut 
-      0, //top cut,
-      32, //width of cut
-      32, //height of cut
-      x * 16 - 8,
-      y * 16 - 18,
-      32,
-      32
-   )
-   }
-   shadow.src = "/images/characters/shadow.png";
+  this.directionInput = new DirectionInput();
+  this.directionInput.init();
+
+  this.startGameLoop();
 
 
-   const hero = new Image();
-   hero.onload = () => {
-     this.ctx.drawImage(
-       hero, 
-       0, //left cut 
-       0, //top cut,
-       32, //width of cut
-       32, //height of cut
-       x * 16 - 8,
-       y * 16 - 18,
-       32,
-       32
-    )
-   }
-   hero.src = "/images/characters/people/hero.png";
-
+  // this.map.startCutscene([
+  //   { type: "changeMap", map: "DemoRoom"}
+  //   // { type: "textMessage", text: "This is the very first message!"}
+  // ])
 
  }
-
 }
